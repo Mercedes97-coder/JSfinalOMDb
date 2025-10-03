cb1c35e47ce1b8cea9ec1ae3a31cf32183835652
mercedes97-coder <mercedesmccann9@gmail.com>
2025-10-03 10:08:07 -0600

    java for  search updated

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 
-const movieListEl = document.querySelector(".movie-list");
 
-async function main() {
-  const movies = await fetch("https://omdbapi.com/?s=fast&apikey=d9398c17");
+document.addEventListener('DOMContentLoaded', () => {
+const movieListEl = document.querySelector(".movie-list");
+const searchInput = document.querySelector('input[type="searchInput"]');
+const placeholder = document.querySelector('.place-holder__img--wrapper');
+
+console.log("movieListEl:" , movieListEl);
+
+movieListEl.style.display = 'none';
+placeholder.style.display = 'flex';
+
+async function searchMovies() {
+  const searchTerm = searchInput.value;
+  if (!searchTerm) {
+    alert('Please type a movie name.');
+    return;
+  }
+  const movies = await fetch(`https://omdbapi.com/?s=${searchTerm}&apikey=d9398c17`);
   const moviesData = await movies.json();
   console.log(moviesData);
-  movieListEl.innerHTML = moviesData.Search.map((movies) =>
-    moviesHTML(movies)
-  ).join("");
-}
-main();
+
+  if (moviesData.Response === "True") {
+    placeholder.style.display = 'none';
+    movieListEl.style.display = 'flex';
+    movieListEl.innerHTML = moviesData.Search.map((movies) => moviesHTML(movies))
+  .join('');
+  } else {
+    alert('No movies found!')
+  }
+};
+
+window.searchMovies = searchMovies;
+searchInput.addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); 
+    searchMovies();
+  }
+});
 
 function moviesHTML(movies) {
-  return ` <div class="movie-card">
+  return ` <div class="movie-card" >
               <div class="movie-card__container">
                 <h3>${movies.Title}</h3>
                 <p><b>Year:</b> ${movies.Year}</p>
@@ -25,3 +52,4 @@ function moviesHTML(movies) {
             </div>`;
 }
 
+});
